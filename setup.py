c6c79a7dd4088d3c5423444225f5f29aaea9dd46
Sriram Madapusi Vasudevan <sriram@klusterkloud.com>
2014-08-20 13:39:14 -0400

    add: dependencies to setup.py
    Per Jorge's comment, we need dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
+import sys
 try:
     from setuptools import setup, find_packages
 except ImportError:
     from ez_setup import use_setuptools
     use_setuptools()
     from setuptools import setup, find_packages
-
-REQUIRES = ['six', 'pecan', 'setuptools >= 1.1.6',
-    'cassandra-driver', 'pymongo']
-
+if sys.version_info[0] == 3:
+    REQUIRES = ['six', 'pecan', 'setuptools >= 1.1.6',
+    'cassandra-driver', 'pymongo', 'msgpack-python', 'asyncio',
+    'aiohttp', 'python-swiftclient']
+else:
+    REQUIRES = ['six', 'pecan', 'setuptools >= 1.1.6',
+    'cassandra-driver', 'pymongo', 'msgpack-python', 'gevent',
+    'python-swiftclient']
 setup(
     name='deuce',
     version='0.1',
