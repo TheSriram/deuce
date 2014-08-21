116b4d61bee6c09b1358c4cbd08ca89a77144956
Sriram Madapusi Vasudevan <sriram@klusterkloud.com>
2014-08-21 11:13:42 -0400

    refactor: setup.py
    REQUIRES is noe extensible depending on python version

--- setup.py
+++ setup.py
@@ -6,23 +6,23 @@ except ImportError:
     from ez_setup import use_setuptools
     use_setuptools()
     from setuptools import setup, find_packages
-if sys.version_info[0] == 3:
-    REQUIRES = ['six', 'pecan', 'setuptools >= 1.1.6',
-    'cassandra-driver', 'pymongo', 'msgpack-python', 'asyncio',
-    'aiohttp', 'python-swiftclient']
 else:
+
     REQUIRES = ['six', 'pecan', 'setuptools >= 1.1.6',
-    'cassandra-driver', 'pymongo', 'msgpack-python', 'gevent',
-    'python-swiftclient']
-setup(
-    name='deuce',
-    version='0.1',
-    description='Deuce - Block-level de-duplication as-a-service',
-    author='Rackspace',
-    author_email='',
-    install_requires=REQUIRES,
-    test_suite='deuce',
-    zip_safe=False,
-    include_package_data=True,
-    packages=find_packages(exclude=['tests'])
-)
+    'cassandra-driver', 'pymongo', 'msgpack-python', 'python-swiftclient']
+    if sys.version_info[0] == 3:
+        REQUIRES.extend(['asyncio', 'aiohttp'])
+    else:
+        REQUIRES.extend(['gevent'])
+    setup(
+        name='deuce',
+        version='0.1',
+        description='Deuce - Block-level de-duplication as-a-service',
+        author='Rackspace',
+        author_email='',
+        install_requires=REQUIRES,
+        test_suite='deuce',
+        zip_safe=False,
+        include_package_data=True,
+        packages=find_packages(exclude=['tests'])
+    )
